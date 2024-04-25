7a713eeaa95e5875e62e1975e802089472df93b0
abuwcheek <abdulloistamov1034@gmail.com>
2024-04-25 17:14:36 +0500

    index.html

--- apps/product/context_processor.py
+++ apps/product/context_processor.py
@@ -2,12 +2,14 @@ from .models import Category
 
 
 def category_context(request):
-     categories = Category.objects.all().filter(is_active=True)
-     mega_menu = categories.filter(parent__isnull=True)[:3]
+     categories = Category.objects.all().filter(is_active=True, parent=None)
+     mega_menu = categories[:3]
+     last_child_ctg = Category.objects.all().filter(is_active=True, children__isnull=True)
 
      context={
           'categories': categories,
-          'mega_menu': mega_menu
+          'mega_menu': mega_menu,
+          'last_child_ctg': last_child_ctg,
      }
 
      return context
--- apps/product/views.py
+++ apps/product/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Product
+from .models import Product, Brand
 
 
 class HomePageView(View):
     def get(self, request):
-        products = Product.objects.all()
+        products = Product.objects.all().filter(is_active=True)
+        fuatured_products = products.order_by('?')[:16]
+        popular_products = products.order_by('?')[:16]
+        new_added_product = products.order_by('-created_at')
+        new_arrivals_products = products.order_by('-updated_at')
+        brands = Brand.objects.all().filter(is_active=True)
+        monthly_best_sell_featured = products.order_by('-percentage')
+        monthly_best_sell_popular = products.order_by('-created_at')
+        monthly_best_sell_new_add = products.order_by('-created_at')
+        foot_deals_outlet = products.order_by('?')[:3]
+        foot_top_selling = products.order_by('?')[:3]
+        foot_hot_releases = products.order_by('?')[:3]
+
         context = {
-            'products': products
+            'products': products,
+            'fuatured_products': fuatured_products,
+            'popular_products': popular_products,
+            'new_added_product': new_added_product,
+            'new_arrivals_products' : new_arrivals_products,
+            'brands': brands,
+            'monthly_best_sell_products': monthly_best_sell_featured,
+            'monthly_best_sell_popular': monthly_best_sell_popular,
+            'monthly_best_sell_new_add': monthly_best_sell_new_add,
+            'foot_deals_outlet': foot_deals_outlet,
+            'foot_top_selling': foot_top_selling,
+            'foot_hot_releases': foot_hot_releases,
         }
         return render(request, 'products/index.html', context)
+
+
